5d19424dfac1cdf19f7f4a35528947343ddd6f33
Enea Pesaresi <94395110+eneaqupovisione@users.noreply.github.com>
2022-11-10 13:57:58 +0000

    a

--- shampoosales.py
+++
@@ -1,8 +0,0 @@
-#apro il file
-my_file = open ("shampoo_sales.csv" , "r")
-#stampo il file a schermo (solo i primi 100 caratteri)
-print (my_file.read()[0:100])
-#chiudo il file
-my_file.close()
-
-
