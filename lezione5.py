f4297a41ed56a6be471a17e7b67e0a39de7adcd4
Enea Pesaresi <94395110+eneaqupovisione@users.noreply.github.com>
2022-11-28 21:34:06 +0000

    lezione 5

--- lezione5.py
+++ lezione5.py
@@ -1,6 +1,18 @@
-class = CSVFile ():
-    def __init__(self, name):
-    try:
-        self = self.name
-    except Exception as problem
+class CSVFile ():
+    def __init__(self, name): 
+        self.name = name
+        try: 
+            with open (self.name) as test1:
+                test1.readlines()
+        except Exception:
+            print("Errore: file mancante")
+
+    def get_data (self):
+        risultato = []
+        with open(self.name) as my_file:
+            for line in my_file:
+                elements = line.strip('\n').split(',')
+                if elements[0] != 'Date':
+                    risultato.append(elements)
+        return risultato
     
